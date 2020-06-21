dbd40722010406ec8ad752968b8869f927e13944
Saumyadip Biswas <saumyadipbiswas@live.com>
2020-06-21 06:20:32 +0530

    Update routes.js
    removed console

--- route/routes.js
+++ route/routes.js
@@ -12,42 +12,11 @@ router.get('/', async (req, res) => {
     }
 });
 
-// router.post('/register', async (req, res) => {
-//     try {
-//         let { name, email, phone, message } = req.body;
-//         fs.readFile(db, 'utf8', (err, jsonString) => {
-//             if (err) {
-//                 return res.send({ message: 'error with json db', data: err });
-//             } else {
-//                 let arrayOfObjects = JSON.parse(jsonString);
-//                 last = Object.keys(arrayOfObjects.table)[Object.keys(arrayOfObjects.table).length - 1];
-//                 arrayOfObjects.table.push({
-//                     _id: typeof last !== "undefined" ? parseInt(last) + 1 : 0,
-//                     name: name,
-//                     email: email,
-//                     phone: phone,
-//                     message: message
-//                 })
-//                 fs.writeFile(db, JSON.stringify(arrayOfObjects), (err) => {
-//                     if (err) {
-//                         return res.send({ message: 'error with json db', data: err });
-//                     } else {
-//                         return res.send({ message: ' you have been successfully registared ', data: { name, email, phone, message } });
-//                     }
-//                 });
-//             }
-//         });
-//     } catch (err) {
-//         console.log(err);
-//         return res.send({ message: 'server error', data: err });
-//     }
-// });
-
 router.get('/all-users', async (req, res) => {
     try {
         fs.readFile(db, 'utf8', (err, jsonString) => {
             if (err) {
-                //return res.send({ message: 'error with json db', data: err });
+                return res.send({ message: 'error with json db', data: err });
             } else { 
                 let arrayOfObjects = JSON.parse(jsonString);
                 return res.send({ message: 'all users', data: arrayOfObjects });
